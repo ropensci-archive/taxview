b21c10dc1f2cc855d90cd6a4e2fb440c63d46da2
Scott Chamberlain <myrmecocystus@gmail.com>
2020-07-20 16:32:17 -0700

    mark tv_viz as not working yet

--- R/tv_viz.R
+++ R/tv_viz.R
@@ -1,19 +1,19 @@
 #' Visualize data with respect to taxonomy
-#' 
+#'
 #' @export
 #' @param x An object of class `tv_summary`
 #' @return opens interactive vis
 #' @examples
 #' x <- system.file("examples/plant_spp.csv", package = "taxview")
 #' dat <- tibble::as_tibble(
-#'  data.table::fread(x, stringsAsFactors = FALSE, 
+#'  data.table::fread(x, stringsAsFactors = FALSE,
 #'    data.table = FALSE))
 #' out <- tv_clean_ids(x, ids = dat$id, db = "ncbi")
 #' (res <- tv_summarise(out))
 #' tv_viz(res)
 tv_viz <- function(x) {
-    # must be of class tv_summary
-    assert(x, "tv_summary")
-    stop("not working yet")
-    return(x)
+  # must be of class tv_summary
+  assert(x, "tv_summary")
+  stop("not working yet")
+  return(x)
 }
