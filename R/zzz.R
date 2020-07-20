f41b60fe1bff450a311af30fdbdad24b7268cc7f
Scott Chamberlain <myrmecocystus@gmail.com>
2020-07-20 16:38:10 -0700

    fix assert() fxn

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 cl <- function(x) Filter(Negate(is.null), x)
 assert <- function(x, y) {
   if (!is.null(x)) {
-    if (!class(x) %in% y) {
+    if (!inherits(x, y)) {
       stop(deparse(substitute(x)), " must be of class ",
            paste0(y, collapse = ", "), call. = FALSE)
     }
